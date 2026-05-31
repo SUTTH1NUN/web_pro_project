4e320603b6b09860398a43afa4c69383b24aac2a
SUTTH1NUN <art.s120549@gmail.com>
2026-06-01 02:01:17 +0700

    refactor: remove unused project configuration files

--- .next/dev/static/chunks/pages/_app.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/[root-of-the-server]__0bjzozx._.js",
-  "static/chunks/[root-of-the-server]__0c0okpg._.js",
-  "static/chunks/pages__app_07xvfw~._.js",
-  "static/chunks/turbopack-pages__app_0h2s30g._.js"
-])
